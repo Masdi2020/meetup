f4ddb9e89e3a59b8c0f9ba90eb66df3a7560b46d
Masdi2020 <dimasramadhani876@gmail.com>
2026-07-22 15:08:00 +0800

    feat: create migration database

---
+++ database/migrations/2026_07_22_064547_create_bookings_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('room_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->date('date');
+
+            $table->time('start_time');
+            $table->time('end_time');
+
+            $table->string('title');
+
+            $table->integer('participants');
+
+            $table->foreignId('status_id')
+                ->constrained('booking_statuses');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
